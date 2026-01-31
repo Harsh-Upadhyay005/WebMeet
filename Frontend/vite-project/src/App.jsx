3097877a2beb9525d35bccd938af980f548cff4b
Harsh Upadhyay <uharsh328@gmail.com>
2026-01-31 16:11:12 +0530

    feat: implement loading state with PageLoader component and update routing logic

--- Frontend/vite-project/src/App.jsx
+++ Frontend/vite-project/src/App.jsx
@@ -2,40 +2,46 @@ import React from 'react'
 import { Routes, Route, Navigate } from 'react-router-dom'
 
 import HomePage from './pages/HomePage.jsx'
-import SignupPage from './pages/SignupPage.jsx'
+import SignUpPage from './pages/SignUpPage.jsx'
 import LoginPage from './pages/LoginPage.jsx'
 import NotificationPage from './pages/NotificationPage.jsx'
 import CallPage from './pages/CallPage.jsx'
 import ChatPage from './pages/ChatPage.jsx'
 import OnboardingPage from './pages/OnboardingPage.jsx'
 import { Toaster } from 'react-hot-toast';
-import { useQuery } from '@tanstack/react-query'
-import { axiosInstance } from './lib/axios.js'
+import useAuthUser from './hooks/useAuthUser.js'
+import PageLoader from './components/PageLoader.jsx'
+
 
 const App = () => {
   // tanstack query can be used here for global data fetching
-  const {data: authData} = useQuery({
-    queryKey: ['authUser'],
-    queryFn: async () => {
-      const response = await axiosInstance.get('/api/auth/me');
-      return response.data;
-    },
-    retry: false, // auth check
-  });
+ const {isLoading, authUser} = useAuthUser()
 
-  const authUser = authData?.user || null;
+ const isAuthenticated = Boolean(authUser)
+ const isOnboarded = authUser?.isOnboarded
 
+  if(isLoading){
+    return (
+      <PageLoader />
+    )
+  }
 
   return (
     <div className='h-screen' data-theme="coffee">
       <Routes>
-        <Route path='/' element={authUser ? <HomePage /> : <Navigate to="/login" />} />
-        <Route path='/signup' element={!authUser ? <SignupPage /> : <Navigate to="/" />} />
-        <Route path='/login' element={!authUser ? <LoginPage /> : <Navigate to="/" />} />
-        <Route path='/notification' element={!authUser ? <NotificationPage /> : <Navigate to="/login" />} />
-        <Route path='/call' element={authUser ? <CallPage /> : <Navigate to="/login" />} />
-        <Route path='/chat' element={authUser ? <ChatPage /> : <Navigate to="/login" />} />
-        <Route path='/onboarding' element={authUser ? <OnboardingPage /> : <Navigate to="/login" />} />
+        <Route path='/' element={isAuthenticated && isOnboarded ? (
+          <HomePage />
+        ) : (
+          <Navigate to = {isAuthenticated ? "/onboarding" : "/login"} />
+        )
+       } 
+       />
+        <Route path='/signup' element={!isAuthenticated ? <SignUpPage /> : <Navigate to="/" />} />
+        <Route path='/login' element={!isAuthenticated ? <LoginPage /> : <Navigate to="/" />} />
+        <Route path='/notification' element={!isAuthenticated ? <NotificationPage /> : <Navigate to="/login" />} />
+        <Route path='/call' element={isAuthenticated ? <CallPage /> : <Navigate to="/login" />} />
+        <Route path='/chat' element={isAuthenticated ? <ChatPage /> : <Navigate to="/login" />} />
+        <Route path='/onboarding' element={isAuthenticated ? <OnboardingPage /> : <Navigate to="/login" />} />
 
       </Routes>
       <Toaster />
