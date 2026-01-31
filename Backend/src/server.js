3e7d097c0d33aa0817aa6dfc40ba9daa845575d6
Harsh Upadhyay <uharsh328@gmail.com>
2026-01-31 07:03:33 +0530

    feat: add chat routes and user routes to server configuration

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -2,6 +2,12 @@ import express from 'express';
 import dotenv from 'dotenv';
 
 import authRoutes from './routes/auth.route.js';
+import userRoutes from './routes/user.route.js';
+import chatRoutes from './routes/chat.route.js';
+
+
+
+
 import {connetdb} from "./lib/db.js";
 import cookieParser from 'cookie-parser';
 
@@ -16,7 +22,8 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use('/api/auth', authRoutes);
-
+app.use('/api/users', userRoutes); 
+app.use("/api/chat", chatRoutes); // Use chatRoutes for chat-related endpoints
 app.listen (PORT, () => {
     console.log(`Server is running on port ${PORT}`);
     connetdb();
