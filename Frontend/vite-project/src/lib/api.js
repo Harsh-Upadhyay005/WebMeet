4a9cdb48e9403382ecf8975a1f7ecf2a13aa48f3
Harsh Upadhyay <uharsh328@gmail.com>
2026-02-01 00:02:43 +0530

    feat: add login function to api for user authentication

--- Frontend/vite-project/src/lib/api.js
+++ Frontend/vite-project/src/lib/api.js
@@ -4,6 +4,11 @@ export const signup = async (signupData) => {
       const response = await axiosInstance.post('/api/auth/signup', signupData)
       return response.data
     };
+    
+export const login = async (loginData) => {
+      const response = await axiosInstance.post('/api/auth/login', loginData)
+      return response.data
+    };
 
 export const getAuthUser = async () => {
       const response = await axiosInstance.get('/api/auth/me');
