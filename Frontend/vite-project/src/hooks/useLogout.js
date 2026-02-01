54433244040be6e8373f8676f87edfa7c80a3683
Harsh Upadhyay <uharsh328@gmail.com>
2026-02-01 16:18:33 +0530

    feat: implement useLogout hook for user logout functionality

---
+++ Frontend/vite-project/src/hooks/useLogout.js
@@ -0,0 +1,30 @@
+import { logout } from "../lib/api";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import toast from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
+
+const useLogout = () => {
+    const queryClient = useQueryClient();
+    const navigate = useNavigate();
+
+    const {
+    mutate: logoutMutation,
+    isPending,
+    error,
+} = useMutation({
+        mutationFn: logout,
+        onSuccess: () => {
+            toast.success("Logged out successfully");
+            queryClient.setQueryData(["authUser"], null);
+            queryClient.invalidateQueries({ queryKey: ["authUser"] });
+            navigate("/login");
+        },
+        onError: (error) => {
+            console.error("Logout error:", error);
+        },
+    });
+
+    return { logoutMutation, isPending, error };
+};
+
+export default useLogout;
