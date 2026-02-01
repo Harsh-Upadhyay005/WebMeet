c513acf32e5e428df320f9ddee021e6939a78703
Harsh Upadhyay <uharsh328@gmail.com>
2026-02-02 02:19:14 +0530

    Fix frontend to backend connection in production

--- Frontend/vite-project/src/lib/axios.js
+++ Frontend/vite-project/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = import.meta.env.MODE ==="development" ? (import.meta.env.VITE_API_BASE_URL || 'http://localhost:3000') : '/api';
+const BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:3000';
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
   withCredentials: true,
