d4e78686c3490e7af0db47300ca7517b1faca995
Harsh Upadhyay <uharsh328@gmail.com>
2026-07-04 08:56:39 +0530

    feat: update default theme in useThemeStore to 'light' for consistency

--- Frontend/vite-project/src/store/useThemeStore.js
+++ Frontend/vite-project/src/store/useThemeStore.js
@@ -1,7 +1,7 @@
 import { create } from 'zustand'
 
 export const useThemeStore = create((set) => ({
-    theme: localStorage.getItem("WebMeet-Theme") || "sunset",
+    theme: localStorage.getItem("WebMeet-Theme") || "light",
     setTheme: (newTheme) => {
       localStorage.setItem("WebMeet-Theme", newTheme);
       set({ theme: newTheme });
